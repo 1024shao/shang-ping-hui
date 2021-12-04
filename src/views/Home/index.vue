82c69a12a0096499fb3419372fe4e80885774982
1024shao <3059680677@qq.com>
2021-12-04 09:47:52 +0800

    style:rank list recommend

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <TypeNav />
+    <ListContainer />
+    <TodayRecommend />
+    <Rank />
   </div>
 </template>
 
 <script>
+import ListContainer from './ListContainer'
+import TodayRecommend from './TodayRecommend'
+import Rank from './Rank'
 export default {
-
+  name: 'Home',
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank
+  }
 }
 </script>
 
