ffab1f920996bb194fe71543754c90ea6c09570e
1024shao <3059680677@qq.com>
2021-12-06 21:54:29 +0800

    style:分页器

---
+++ src/components/Pagination/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="pagination">
+    <button>上一页</button>
+    <button>1</button>
+    <button>···</button>
+
+    <button>3</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>7</button>
+
+    <button>···</button>
+    <button>9</button>
+    <button>下 一页</button>
+
+    <button style="margin-left: 30px">共 60 条</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+}
+</script>
+
+<style lang="less" scoped>
+.pagination {
+  text-align: center;
+  button {
+    margin: 0 5px;
+    background-color: #f4f4f5;
+    color: #606266;
+    outline: none;
+    border-radius: 2px;
+    padding: 0 4px;
+    vertical-align: top;
+    display: inline-block;
+    font-size: 13px;
+    min-width: 35.5px;
+    height: 28px;
+    line-height: 28px;
+    cursor: pointer;
+    box-sizing: border-box;
+    text-align: center;
+    border: 0;
+
+    &[disabled] {
+      color: #c0c4cc;
+      cursor: not-allowed;
+    }
+
+    &.active {
+      cursor: not-allowed;
+      background-color: #409eff;
+      color: #fff;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { router } from './router'
 import store from './store'
 import TypeNav from '@/common/TypeNav'
 import Carousel from '@/components/Carousel';
+import Pagination from '@/components/Pagination';
 import '@/mock/mockServer'
 
 // 引入样式文件
@@ -11,6 +12,7 @@ import 'swiper/css/swiper.css'
 
 Vue.component(TypeNav.name, TypeNav)
 Vue.component(Carousel.name, Carousel)
+Vue.component(Pagination.name, Pagination)
 
 
 new Vue({
