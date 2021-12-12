d704fd5acc61882e2bfcf2ec16216eb022ba7f63
1024shao <3059680677@qq.com>
2021-12-12 15:05:10 +0800

    feat: 用户地址信息展示

--- src/views/Trade/index.vue
+++ src/views/Trade/index.vue
@@ -3,28 +3,12 @@
     <h3 class="title">填写并核对订单信息</h3>
     <div class="content">
       <h5 class="receive">收件人信息</h5>
-      <div class="address clearFix">
-        <span class="username selected">张三</span>
-        <p>
-          <span class="s1">北京市昌平区宏福科技园综合楼6层</span>
-          <span class="s2">15010658793</span>
-          <span class="s3">默认地址</span>
-        </p>
-      </div>
-      <div class="address clearFix">
-        <span class="username selected">李四</span>
-        <p>
-          <span class="s1">北京市昌平区宏福科技园综合楼6层</span>
-          <span class="s2">13590909098</span>
-          <span class="s3">默认地址</span>
-        </p>
-      </div>
-      <div class="address clearFix">
-        <span class="username selected">王五</span>
-        <p>
-          <span class="s1">北京市昌平区宏福科技园综合楼6层</span>
-          <span class="s2">18012340987</span>
-          <span class="s3">默认地址</span>
+      <div class="address clearFix" v-for="item in addressInfo" :key="item.id">
+        <span class="username " :class="{selected:item.isDefault==='1'}">{{item.consignee}}</span>
+        <p @click="changeDefault(item)">
+          <span class="s1">{{item.fullAddress}}</span>
+          <span class="s2">{{item.phoneNum}}</span>
+          <span class="s3" v-show="item.isDefault==='1'"> 默认地址</span>
         </p>
       </div>
       <div class="line"></div>
@@ -108,9 +92,9 @@
       <div class="price">应付金额:　<span>¥5399.00</span></div>
       <div class="receiveInfo">
         寄送至:
-        <span>北京市昌平区宏福科技园综合楼6层</span>
-        收货人：<span>张三</span>
-        <span>15010658793</span>
+        <span>{{selectAddress.fullAddress}}</span>
+        收货人：<span>{{selectAddress.consignee}}</span>
+        <span>{{selectAddress.phoneNum}}</span>
       </div>
     </div>
     <div class="sub clearFix">
@@ -120,11 +104,26 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'Trade',
   created() {
     this.$store.dispatch('getAddressInfo')
     this.$store.dispatch('getOrderInfo')
+  },
+  computed: {
+    ...mapState({
+      addressInfo: state => state.trade.addressInfo
+    }),
+    selectAddress() {
+      return this.addressInfo.find(item => item.isDefault == '1')
+    }
+  },
+  methods: {
+    changeDefault(item) {
+      this.addressInfo.forEach(address => address.isDefault = '0')
+      item.isDefault = '1'
+    }
   }
 }
 </script>
